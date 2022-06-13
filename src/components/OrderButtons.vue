cf61f979aa8782495641f50a96e7a1cd90f2837b
jeysonlr <jeysonlr@gmail.com>
2022-06-13 19:47:33 -0300

    feat: solucao para exercicios propostos em video aula

--- src/components/OrderButtons.vue
+++ src/components/OrderButtons.vue
@@ -11,14 +11,12 @@ const emit = defineEmits(['sort-by'])
       @click="emit('sort-by', 'id')"
     >
     1-9
-      <!-- <IconOrderNumber /> -->
     </button>
     <button
       class="border-l border-gray-500 bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-3 rounded-r"
       @click="emit('sort-by', 'name')"
     >
     A-Z
-      <!-- <IconOrderAlpha /> -->
     </button>
   </div>
 </template>
